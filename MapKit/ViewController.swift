4d0172c463cd8c5c000453620b5f72591e2cfe99
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-16 11:40:07 +0300

    feat: initialize MapKit application

---
+++ MapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKit
+//
+//  Created by Lee Sangoroh on 16/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

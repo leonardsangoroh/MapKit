fdc70677be01a5c7f0b302c7fdb1fe05fdebbcff
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-17 00:18:09 +0300

    feat: add capital.swift

--- MapKitProject/Capital.swift
+++ MapKitProject/Capital.swift
@@ -6,7 +6,17 @@
 //
 
 import UIKit
+// import because MKAnnotation & CLLocationCoordinate2D are defined in MapKit
+import MapKit
 
-class Capital: NSObject {
-
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
 }
